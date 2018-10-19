36ebc999cd3ddd4290accbe73f04ee2f403de730
xVivix <43880779+xVivix@users.noreply.github.com>
2018-10-19 14:47:35 +0200

    .v1.1_19.10_Nav_BS_IMG

--- PHP_Klasses/Nav.php
+++ PHP_Klasses/Nav.php
@@ -12,6 +12,9 @@ class Nav {
      */
     function getNavHTML(){
         echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+        <img src="/WebsiteDipl_V1/pictures/testimage.png" width="200" height="60" alt="">
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
