8d1cd432bf4d27c47209b57284d6e934328945f3
xVivix <43880779+xVivix@users.noreply.github.com>
2018-11-04 16:27:48 +0100

    04.11.2018 Nav

--- PHP_Klasses/Nav.php
+++ PHP_Klasses/Nav.php
@@ -12,26 +12,29 @@ class Nav {
      */
     function getNavHTML(){
         echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-        <img src="/WebsiteDipl_V1/pictures/testimage.png" width="200" height="60" alt="">
+        <a class="navbar-brand" href="index.php">
+        <img src="/WebsiteDipl_V2/pictures/logo_v1.png" height="76" wight="174"  alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-        <a class="navbar-brand" href="index.php">Home</a>
+        <a class="navbar-brand" href="index.php">HOME</a>
         <li class="nav-item">
-        <a class="nav-link" href="#">Text2<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">&Uuml;BER UNS<span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" href="#">Text3</a>
+        <a class="nav-link" href="#">KONTAKT</a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Text4</a>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">DIENSTLEISTUNGEN</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">ERDBEWEGUNG</a>
+          <a class="dropdown-item" href="#">PFLASTEREI</a>
+          <a class="dropdown-item" href="#">WARENHANDEL</a>
+        </div>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="#">Test5</a>
         </li>
         </ul>
         </div>
